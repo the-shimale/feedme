e7b88547bb65f4dc65284ff695607a20accc8239
Able Shi <zaixing.a.shi@vanderbilt.edu>
2018-12-11 18:05:14 -0600

    added rating input

---
+++ public/index.js
@@ -0,0 +1,6 @@
+
+const onLoad = () => {
+	// $("#rating").slider({ min: 1, max: 5, value: [1, 5], focus: true });
+	// let ratingSlider = new Slider("#rating", { min: 1, max: 5, value: [1, 5], focus: true });
+
+};
